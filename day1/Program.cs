5be6611a31ee876cf0e55d88a8a4c360c4ab7339
21ulwe <21ulwe@stockholmscience.se>
2022-12-01 11:42:50 +0100

    day 1

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,8 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string[] lines = System.IO.File.ReadAllLines(@"C:\Programmering\Projekt\AOC\day1pt1\input.txt");
-List<ElfInventory> Inventory = new List<ElfInventory>();
 int highestValue = 0;
 List<int> elfInventory = new List<int>();
+
+List<ElfInventory> Inventory = new List<ElfInventory>();
+List<int> SortedInventorySum = new List<int>();
+List<int> InventorySum = new List<int>();
+
+
+
+
 
 foreach (var item in lines)
 {
@@ -31,6 +38,21 @@ foreach (var item in Inventory)
 
 System.Console.WriteLine($"highestValue: {highestValue}");
 
+
+foreach (var item in Inventory)
+{
+    InventorySum.Add(item.Inventory.Sum());
+}
+
+InventorySum.Sort();
+int count = InventorySum.Count() -1;
+int sum = 0;
+System.Console.WriteLine(count);
+for (int i = count ; i > count -3 ; i--)
+{
+    sum += InventorySum[i];
+}
+System.Console.WriteLine($"sum{sum}");
 
 public class ElfInventory
 {
